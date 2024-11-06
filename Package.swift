23226ba64ea58390ba38b00f4701ba2633747f23
timmy <luongvantiem@gmail.com>
2024-11-06 17:37:59 +0700

    feat: update name

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .binaryTarget(
-            name: "BinarySPM",
+            name: "TestFramework",
             url: "https://github.com/jimmyluong/BinarySPM/archive/refs/tags/0.1.0.zip",
             checksum: "8c356979957de69d419743ad3203f890f054203a62f5c5e7b7a7e702a111de65"
         )
