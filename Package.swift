1eadbb71cdc543ccf5a36533185235074c0a4fe8
timmy <luongvantiem@gmail.com>
2024-11-06 17:47:32 +0700

    feat: update name 2

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "TestFramework",
+    name: "BinarySPM",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "TestFramework",
+            name: "BinarySPM",
             targets: ["TestFramework"]),
     ],
     targets: [
