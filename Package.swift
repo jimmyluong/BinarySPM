afe0199cf0f1502bfdeb44a47d3fe294e09266d5
timmy <luongvantiem@gmail.com>
2024-11-06 17:52:02 +0700

    fix: package mainfest

--- Package.swift
+++ Package.swift
@@ -9,11 +9,12 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "BinarySPM",
-            targets: ["TestFramework"]),
+            targets: ["BinarySPM", "TestFramework"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
+        .target(name: "BinarySPM", dependencies: []),
         .binaryTarget(
             name: "TestFramework",
             url: "https://github.com/jimmyluong/BinarySPM/archive/refs/tags/0.1.0.zip",
